062e827087f3b80b4fe9beaaf95b48231195d459
Victor Samuel <vitorcatoneferreira@gmail.com>
2026-02-13 14:55:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4939730413-92939",
+  "appId": "1:18358455050:web:7bd1483dee20ea866948f3",
+  "apiKey": "AIzaSyCvSx5KyIvyL0tOk0Q3WEXurmYsg6Q3SVQ",
+  "authDomain": "studio-4939730413-92939.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "18358455050"
+};
